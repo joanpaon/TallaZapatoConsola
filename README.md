d01c87bc2ca9f072552af42ec667365a98db861b
José A. Pacheco Ondoño <joanpaon@gmail.com>
2017-10-05 14:22:59 +0200

    :tada: First version

---
+++ README.md
@@ -0,0 +1,11 @@
+<pre>
+Project .....: TallaZapatoConsola
+Summary .....: Java 8 | Netbeans 8.2 | Programas - Entrada Consola #01
+Creation ....: 26/10/2016
+Modification : 
+Version .....: 
+Author ......: José A. Pacheco Ondoño - joanpaon@gmail.com
+Platform ....: Java 8
+IDE .........: NetBeans 8.2
+Remote ......: https://github.com/joanpaon/TallaZapatoConsola.git
+</pre>
---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,55 @@
+/* 
+ * Copyright 2016 José A. Pacheco Ondoño - joanpon@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author José A. Pacheco Ondoño - joanpon@gmail.com
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Instanciar Scanner
+        Scanner scn = new Scanner(System.in);
+        
+        // Variable
+        int talla;
+
+        // Cuarentena
+        try {
+            // Leer dato
+            System.out.print("Talla de zapato ..........: ");
+            talla = scn.nextInt();
+
+            // Borrar buffer
+            scn.nextLine();
+            
+            // Mensaje
+            System.out.printf("Talla de zapato ..........: %d\n", talla);
+        } catch (Exception e) {
+            // Mensaje
+            System.out.println("Entrada incorrecta");
+
+            // Borrar buffer
+            scn.nextLine();
+        }
+    }
+}
